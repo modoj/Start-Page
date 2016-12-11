b3aa41807882bb08ff12ec08bebcb1c69c2f6e16
Adam Thompson <adam@serialphotog.com>
2016-12-11 07:56:46 -0500

    Converted to sass :)

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					style: 'pretty',
+					compass: false
+				},
+
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass'],
+				options: {
+					livereload: true,
+				},
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['watch']);
+	
+};
